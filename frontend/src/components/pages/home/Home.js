8dc5bf0c76bb20c87c0ffcad85e29015df84b8a7
tienshake <tientrancntt@gmail.com>
2022-04-02 10:48:57 +0700

    fix stick home

--- frontend/src/components/pages/home/Home.js
+++ frontend/src/components/pages/home/Home.js
@@ -5,16 +5,16 @@ import React, { useState, useEffect } from 'react';
 import NavBarApp from "../../nav/NavBarApp";
 import './Home.scss';
 import Footer from '../../footer/Footer';
+import Sticked from '../../sticked/Sticked';
 const Home = () => {
     const [titleSearch, setTitleSearch] = useState("");
+    const pathName = window.location.pathname
     const handleChangeTitleSearch = (text) => {
         setTitleSearch(text)
-        console.log(titleSearch)
     }
     return (
         <div className="home__container">
-            <img className="home__sticked home__sticked--left" src="https://cdn.tgdd.vn/2022/03/banner/Trai1-80x275-1.png" alt="" />
-            <img className="home__sticked home__sticked--right" src="https://cdn.tgdd.vn/2022/03/banner/Phai-80x275-1.png" alt="" />
+
             <NavBarApp handleChangeTitleSearch={handleChangeTitleSearch} />
             <Outlet context={{ titleSearch }} />
             <Footer />
