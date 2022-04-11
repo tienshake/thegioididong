9bc4817a984f8d68e5a35349210b3f7610d3ee39
Thanh Trưong <truongnguyendep@gmail.com>
2022-04-11 20:37:40 +0700

    update detail

--- frontend/src/components/pages/detailItem/DetailItem.js
+++ frontend/src/components/pages/detailItem/DetailItem.js
@@ -14,6 +14,7 @@ import Policy from './Policy';
 import Configuration from './Configuration';
 import Post from './Post';
 import Modal from './modal/Modal';
+import Evaluate from './Evaluate';
 const DetailItem = () => {
     const { id } = useParams();
     const [product, setProduct] = useState({});
@@ -111,6 +112,10 @@ const DetailItem = () => {
                         markdown={markdown}
                         openModal={openModal}
                     />
+
+
+                    {/* Đánh giá SP */}
+                    <Evaluate/>
 
                 </div>
                 <div className="phone__detail-product-right">
---
+++ frontend/src/components/pages/detailItem/Evaluate.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import { FaRegComment } from "react-icons/fa";
+import {AiOutlineStar, AiTwotoneStar, AiOutlineLike } from "react-icons/ai";
+import { BsCheckCircle } from "react-icons/bs";
+
+export default function Evaluate() {
+    return (
+        <>
+            {/* Đánh giá sản phẩm */}
+            <div style={{ marginTop: '50px', border: '1px solid #E0E0E0', borderRadius: '9px' }} className='container wrap__evaluate__product'>
+                <h5 style={{ paddingTop: '20px', paddingBottom: '9px' }}>Đánh giá Tai nghe Bluetooth AirPods 2 Apple MV7N2</h5>
+                <div className='header__evaluate'>
+                    <div className='item__1'>
+                        <div style={{ display: 'flex', height: '30px' }}>
+                            <p style={{ width: '13%', fontSize: '18px', color: '#FE8C23' }}><b>4.0</b></p>
+                            <p style={{ marginRight: '5px' }}>
+                                <AiOutlineStar style={{ fontSize: '18px', color: '#FE8C23' }} />
+                                <AiOutlineStar style={{ fontSize: '18px', color: '#FE8C23' }} />
+                                <AiOutlineStar style={{ fontSize: '18px', color: '#FE8C23' }} />
+                                <AiOutlineStar style={{ fontSize: '18px', color: '#FE8C23' }} />
+                                <AiOutlineStar style={{ fontSize: '18px', color: '#FE8C23' }} />
+                            </p>
+                            <p style={{ fontSize: '15px', marginTop: '1px' }}>801 đánh giá</p>
+                        </div>
+
+                        <div style={{ display: 'flex', height: '20px' }}>
+                            <p style={{ marginRight: '7px', fontSize: '14px' }}>5</p>
+                            <AiTwotoneStar style={{ color: '3px', marginTop: '4px', marginRight: '5px' }} />
+                            <p style={{ height: '4px', width: '43%', background: "#FEBC23", marginTop: '10px' }}></p>
+                            <p style={{ height: '4px', width: '100%', background: "#EEEEEE", marginTop: '10px' }}></p>
+                            <p style={{ marginLeft: '10px', fontSize: '14px' }}>41%</p>
+                        </div>
+
+                        <div style={{ display: 'flex', height: '20px' }}>
+                            <p style={{ marginRight: '7px', fontSize: '14px' }}>4</p>
+                            <AiTwotoneStar style={{ color: '3px', marginTop: '4px', marginRight: '5px' }} />
+                            <p style={{ height: '4px', width: '43%', background: "#FEBC23", marginTop: '10px' }}></p>
+                            <p style={{ height: '4px', width: '100%', background: "#EEEEEE", marginTop: '10px' }}></p>
+                            <p style={{ marginLeft: '10px', fontSize: '14px' }}>27%</p>
+                        </div>
+
+                        <div style={{ display: 'flex', height: '20px' }}>
+                            <p style={{ marginRight: '7px', fontSize: '14px' }}>3</p>
+                            <AiTwotoneStar style={{ color: '3px', marginTop: '4px', marginRight: '5px' }} />
+                            <p style={{ height: '4px', width: '43%', background: "#FEBC23", marginTop: '10px' }}></p>
+                            <p style={{ height: '4px', width: '100%', background: "#EEEEEE", marginTop: '10px' }}></p>
+                            <p style={{ marginLeft: '10px', fontSize: '14px' }}>19%</p>
+                        </div>
+
+                        <div style={{ display: 'flex', height: '20px' }}>
+                            <p style={{ marginRight: '7px', fontSize: '14px' }}>2</p>
+                            <AiTwotoneStar style={{ color: '3px', marginTop: '4px', marginRight: '5px' }} />
+                            <p style={{ height: '4px', width: '27%', background: "#FEBC23", marginTop: '10px' }}></p>
+                            <p style={{ height: '4px', width: '100%', background: "#EEEEEE", marginTop: '10px' }}></p>
+                            <p style={{ marginLeft: '10px', fontSize: '14px' }}>6%</p>
+                        </div>
+
+                        <div style={{ display: 'flex', height: '20px' }}>
+                            <p style={{ marginRight: '7px', fontSize: '14px' }}>1</p>
+                            <AiTwotoneStar style={{ color: '3px', marginTop: '4px', marginRight: '5px' }} />
+                            <p style={{ height: '4px', width: '43%', background: "#FEBC23", marginTop: '10px' }}></p>
+                            <p style={{ height: '4px', width: '100%', background: "#EEEEEE", marginTop: '10px' }}></p>
+                            <p style={{ marginLeft: '10px', fontSize: '14px' }}>6%</p>
+                        </div>
+
+                    </div>
+
+                    <p style={{ width: '1px', background: '#E0E0E0' }}></p>
+                    <div className='item__2'>
+                        <img src='https://cdn.tgdd.vn/comment/50957062/514F32DB-108A-42F2-A91D-4CC84A58C487RBZVL.jpeg' alt='' />
+                        <img src='https://cdn.tgdd.vn/comment/50957062/C4A201C8-988C-488D-A022-B1F6C12B3CD40IDUM.jpeg' alt='' />
+                        <img src='https://cdn.tgdd.vn/comment/50758648/imageYD0GN.jpg' alt='' />
+                        <img src='https://cdn.tgdd.vn/comment/50957062/514F32DB-108A-42F2-A91D-4CC84A58C487RBZVL.jpeg' alt='' />
+                        <img src='https://cdn.tgdd.vn/comment/50957062/514F32DB-108A-42F2-A91D-4CC84A58C487RBZVL.jpeg' alt='' />
+                        <img src='https://cdn.tgdd.vn/comment/50957062/514F32DB-108A-42F2-A91D-4CC84A58C487RBZVL.jpeg' alt='' />
+                        <img src='https://cdn.tgdd.vn/comment/50957062/514F32DB-108A-42F2-A91D-4CC84A58C487RBZVL.jpeg' alt='' />
+                        <img src='https://cdn.tgdd.vn/comment/50957062/514F32DB-108A-42F2-A91D-4CC84A58C487RBZVL.jpeg' alt='' />
+                        <img src='https://cdn.tgdd.vn/comment/50957062/514F32DB-108A-42F2-A91D-4CC84A58C487RBZVL.jpeg' alt='' />
+                        <img src='https://cdn.tgdd.vn/comment/50957062/514F32DB-108A-42F2-A91D-4CC84A58C487RBZVL.jpeg' alt='' />
+                    </div>
+                </div>
+
+                <div className='body__evaluate'>
+                    <p style={{ height: '1px', background: '#E0E0E0', marginTop: '20px' }}></p>
+                    <div style={{ display: 'flex', height: '25px' }}>
+                        <p style={{ marginRight: '10px' }}><b>Hạnh Linh</b></p>
+                        <div style={{ color: '#52B858', display: 'flex' }}><BsCheckCircle style={{ fontSize: '14px', marginTop: '3px', marginRight: '6px' }} /> <p style={{ fontSize: '12px' }}>Đã mua tại TGDD</p> </div>
+                    </div>
+
+                    <div style={{ marginRight: '10px' }}>
+                        <AiTwotoneStar style={{ fontSize: '15px', color: '#FE8C23' }} />
+                        <AiTwotoneStar style={{ fontSize: '15px', color: '#FE8C23' }} />
+                        <AiTwotoneStar style={{ fontSize: '15px', color: '#FE8C23' }} />
+                        <AiTwotoneStar style={{ fontSize: '15px', color: '#FE8C23' }} />
+                        <AiTwotoneStar style={{ fontSize: '15px', color: '#FE8C23' }} />
+                    </div>
+
+                    <p style={{ fontSize: '13px' }}>Nhỏ, gọn, tiện ích, âm thanh nhức cái nách! Ban đầu con suy nghĩ về chất lượng,
+                        nhueng eo ui! Nghe nhạc, đàm thoại k có chỗ chê! Nv giao hàng thì thân thiện! Nhiệt tình! Do hàng của hãng Apple nên nhân viên k
+                        thể bóc seal! Cảm giác đc đập hộp nữa chớ! Mua rất đáng $ và đáng tin tưởng từ TGDD
+                    </p>
+
+                    <div className='wrap__like__comment'>
+                        <AiOutlineLike style={{ marginTop: '4px', color: '#2F80ED', marginRight: '2px' }} />
+                        <div>Hữu ích</div>
+                        <FaRegComment style={{ marginTop: '4px', color: '#2F80ED', marginRight: '2px' }} />
+                        <div>Thảo luận</div>
+                        <div>Đã dùng khoản 2 tháng</div>
+                    </div>
+                </div>
+
+                <p style={{ height: '1px', background: '#E0E0E0', marginTop: '20px' }}></p>
+                <div className='group__btn__evaluate'>
+                    <div className='btn__write__evaluate' ><p style={{ marginTop: '8px' }}>Viết Đánh Giá</p></div>
+                    <div className='btn__see__evaluate'><p style={{ marginTop: '8px' }}>Xem Đánh 853 giá</p></div>
+                </div>
+            </div>
+        </>
+    )
+}
